167bfc2de7710009c3c24c34e3472a695a0adafb
shokilevtech <shoukilevtech@gmail.com>
2022-11-03 17:49:11 +0900

    08_3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,19 +11,19 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <!-- タイトルに詳細リンクを挿入 -->
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
+                    <!-- bodyを表示 -->
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
+        
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
-        
-        <!-- タイトルに詳細リンクを挿入 -->
-        <h2 class='title'>
-            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-        </h2>
         
     </body>
 </html>
